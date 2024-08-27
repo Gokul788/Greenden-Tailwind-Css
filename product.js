24a14ea450a206f26828a352402832472a62ac6b
gokul788 <gokul774777@gmail.com>
2024-08-27 13:43:44 +0530

    add js

---
+++ product.js
@@ -0,0 +1,20 @@
+var containerpro = document.getElementById("container-pro")
+var search = document.getElementById("Search")
+var divlist = containerpro.querySelectorAll("div")
+
+
+search.addEventListener("keyup",function(){
+    var entered = search.value.toUpperCase();
+      
+    for( let i=0; i<divlist.length ; i++){
+        
+        var h1list = divlist[i].querySelector("h1").textContent;
+
+        if(h1list.toUpperCase().indexOf(entered)<0){
+            divlist[i].style.display = "none"
+        }
+        else{
+            divlist[i].style.display = "block"
+        }
+    }
+    })
